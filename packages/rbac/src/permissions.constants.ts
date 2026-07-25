b7486c88e697b8e6bb76b31e9f45d3b9a1017157
Fernando L. Gorordo <fgorordodev@gmail.com>
2026-07-25 03:43:20 +0000

    asd

---
+++ packages/rbac/src/permissions.constants.ts
@@ -0,0 +1,23 @@
+export const PERMISSIONS = {
+  USER_CREATE: {
+    name: "user:create",
+    description: "Create users",
+  },
+  USER_READ: {
+    name: "user:read",
+    description: "Read users",
+  },
+  USER_UPDATE: {
+    name: "user:update",
+    description: "Update users",
+  },
+  USER_DELETE: {
+    name: "user:delete",
+    description: "Delete users",
+  },
+} as const;
+
+export type PermissionDefinition =
+  (typeof PERMISSIONS)[keyof typeof PERMISSIONS];
+
+export type PermissionName = PermissionDefinition["name"];
